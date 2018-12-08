e5373f838fd915cdf5aeb0f6905c0df4df51d2d7
folko11 <foltan.milosml@gmail.com>
2018-12-08 21:42:31 +0100

    Oprava mato

--- src/generatorDat/GeneratorDat.java
+++ src/generatorDat/GeneratorDat.java
@@ -63,6 +63,7 @@ public class GeneratorDat {
         }
         //test
         //this.generujObcanov(100000);
+        //this.generujZamestnancov(10);
     }
     
     public void generujObcanov(int pocet){
@@ -165,7 +166,7 @@ public class GeneratorDat {
     }
     
     //generator zamestnancov autor:Mato
-    private void generujZamestnancov(int pocet) {
+    public void generujZamestnancov(int pocet) {
         Object[] osoby = db.getOsoby();
         BufferedWriter writer = null;
         String csvFile = "Zamestnanci.dat";
